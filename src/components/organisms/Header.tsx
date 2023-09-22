43bac04b5cb328520b9a53edcd33a301acfc3bf8
Saulo Araújo <skdtarso@gmail.com>
2023-09-22 11:04:47 -0300

    feat: Add link to github page

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import { MdMenu } from "react-icons/md";
+import { VscGithubInverted } from "react-icons/vsc";
 import { Button, ButtonVariants, Title } from "@atoms";
 import { Drawer } from "./Drawer";
 
@@ -13,13 +14,16 @@ export function Header({}: HeaderProps) {
 
   return (
     <header className="bg-neutral-100">
-      <div className="container flex p-4 items-center gap-4">
+      <div className="flex p-4 items-center gap-4">
         <Button variant={ButtonVariants.FLAT} onClick={() => setIsOpen(true)}>
           <MdMenu size={32}></MdMenu>
         </Button>
         <Drawer isOpen={isOpen} onClose={() => setIsOpen(false)} />
         <Link href="/">
           <Title className="m-0">YADOT</Title>
+        </Link>
+        <Link href="https://github.com/sdtarso/yadot" target="_blank" className="ml-auto" title="Github Page">
+          <VscGithubInverted size={32} />
         </Link>
       </div>
     </header>
